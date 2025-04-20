028c5e77d99e8794004e0e83c787ab018ebdee1a
steinned <>
2025-04-20 20:34:39 +0200

    Remove unused code.

--- Audiograb-dev/Core/Src/sd_emulation.c
+++ Audiograb-dev/Core/Src/sd_emulation.c
@@ -77,38 +77,6 @@ void transfer_SPI_DMA(uint8_t *txbuf, uint8_t *rxbuf, unsigned int trans_len) //
 	LL_SPI_Enable(SD_EMUL_SPI);
 }
 
-
-/*void SPI_TX_RX_complete_callback(/*SPI_HandleTypeDef *hspi)
-{
-	HAL_GPIO_TogglePin(USER_LED2_GPIO_Port, USER_LED2_Pin); //do something
-	if(transmission_length == 1 && state == awaiting_cmd)
-	{
-		uint8_t received_byte = SD_emulator_rx_buffer[0];
-		if((received_byte & SD_START_BITS_MASK) == SD_VALID_START_PATTERN)
-		{
-			state = receiving_cmd;
-			command_num = (received_byte & ~SD_START_BITS_MASK); //the 6 least significant bits are the command number.
-
-			//Receive the rest of the command (5 more bytes).
-			transmission_length = 5;
-			//set_bytes_to_ff(SD_emulator_tx_buffer, transmission_length);
-			//HAL_SPI_TransmitReceive_DMA(hspi, SD_emulator_tx_buffer, SD_emulator_rx_buffer, transmission_length); //replace me
-			return;
-		}
-	}
-	else if(state == receiving_cmd)
-	{
-		command_arg = SD_emulator_rx_buffer[3];		//LSBs of argument
-		command_arg |= (SD_emulator_rx_buffer[2] << 8);
-		command_arg |= (SD_emulator_rx_buffer[1] << 16);
-		command_arg |= (SD_emulator_rx_buffer[0] << 24); //MSBs of argument
-		command_CRC = SD_emulator_rx_buffer[4];
-		SD_command_handler(command_num, command_arg, command_CRC);
-		//Do I place SPI_transfer here or in command_handler?
-		return;
-	}
-}*/
-
 void SD_command_handler(uint8_t num, uint32_t arg, uint8_t crc)
 {
 	switch(num)
@@ -130,12 +98,3 @@ void SD_command_handler(uint8_t num, uint32_t arg, uint8_t crc)
 	}
 }
 
-/*void set_bytes_to_ff(uint8_t *buf, const unsigned int len)
-{
-	if(buf != NULL) {
-		for(uint8_t i = 0; i < len; ++i) //Set all bytes to be transmitted to 0xff
-		{
-			buf[i] = 0xff;
-		}
-	}
-}*/
